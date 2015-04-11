1a79889af65bf73d249cbb3427aff04d15023c0f
lukasgal <lukasgal@seznam.cz>
2015-04-11 13:19:34 +0200

    Generuje úkoly podle času úkolů, tj. rozkládá čas jednotlivým pracovníkům, aby byl splněn úkol a je jedno, kdo na něm pracuje, např: úkol1 (8 hod) - prac1 6 hodin - prac2 2 hodiny.
    run:
    název                     prior cas   stav  zadáno               zavislost            
    
    -> 2.                     80   12   12	02.04.2015          	-> 1.                     50   10   10	01.04.2015          	null
    -> 2.1.                   70    8    8	11.04.2015          	-> 2.                     80   12   12	02.04.2015          	-> 1.                     50   10   10	01.04.2015          	null
    -> 3.                     60    7    7	04.04.2015          	-> 2.                     80   12   12	02.04.2015          	-> 1.                     50   10   10	01.04.2015          	null
    -> 2.2.                   60    8    8	02.04.2015          	-> 2.                     80   12   12	02.04.2015          	-> 1.                     50   10   10	01.04.2015          	null
    -> 2.2.2                  60    8    8	11.04.2015          	-> 2.2.                   60    8    8	02.04.2015          	-> 2.                     80   12   12	02.04.2015          	-> 1.                     50   10   10	01.04.2015          	null
    -> 2.2.3                  60    8    8	11.04.2015          	-> 2.2.2                  60    8    8	11.04.2015          	-> 2.2.                   60    8    8	02.04.2015          	-> 2.                     80   12   12	02.04.2015          	-> 1.                     50   10   10	01.04.2015          	null
    -> 1.                     50   10   10	01.04.2015          	null
    -> 1.1.                   40    8    8	05.04.2015          	-> 1.                     50   10   10	01.04.2015          	null
    
    -> 1.                     50   10   10	01.04.2015          	null
    pracovnik            odprac    
    Prog1                    0
    Prog2                    5
    
    Prog1                    0
    
    ----------------------WORKING DAY 1
    pracovnik  odprac./den		 ukol       zbyva odpracovano
    
    Prog1          8		 1.             2     8
    Prog2          2		 1.             0     2
    Prog2          8		 2.             6     6
    ----------------------
    
    ----------------------WORKING DAY 2
    pracovnik  odprac./den		 ukol       zbyva odpracovano
    
    Prog1          6		 2.             0     6
    Prog1          8		 2.1.           6     2
    Prog2          6		 2.1.           0     6
    Prog2          8		 3.             5     2
    ----------------------
    
    ----------------------WORKING DAY 3
    pracovnik  odprac./den		 ukol       zbyva odpracovano
    
    Prog1          5		 3.             0     5
    Prog1          8		 2.2.           5     3
    Prog2          5		 2.2.           0     5
    Prog2          8		 2.2.2          5     3
    ----------------------
    
    ----------------------WORKING DAY 4
    pracovnik  odprac./den		 ukol       zbyva odpracovano
    
    Prog1          5		 2.2.2          0     5
    Prog1          8		 2.2.3          5     3
    Prog2          5		 2.2.3          0     5
    Prog2          8		 1.1.           5     3
    ----------------------
    
    ----------------------WORKING DAY 5
    pracovnik  odprac./den		 ukol       zbyva odpracovano
    
    Prog1          5		 1.1.           0     5
    ----------------------
    
    ------------------
    null
    pracovnik            odprac    
    Prog1                    5
    Prog2                    0

---
+++ src/Entity/WorkingDay.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Entity;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ *
+ * @author Lukáš Gál
+ */
+public class WorkingDay {
+    
+    private Integer id;
+    private Date date;
+    private final ArrayList<SolvingTask> solved = new ArrayList<>();
+
+    public WorkingDay(Integer id) {
+        this.id = id;
+    }
+    public void addSolvingTask(SolvingTask st){
+        solved.add(st);
+    
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("----------------------\n");
+        sb.append("WORKING DAY "+id+"\n");
+        sb.append(String.format("%-10s %-5s\t\t %-10s %-5s %-5s\n", "pracovnik","odprac./den", "ukol","zbyva","odpracovano"));
+        return sb.toString();
+    }
+    
+    
+    
+}
